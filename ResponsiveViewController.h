401b31f19f6b61ddb42aff621bf03797e41518be
Carl Parker <Carl.Parker@hbo.com>
2015-04-25 16:27:36 -0700

    commit initial set of files

---
+++ ResponsiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResponsiveViewController.h
+//  layout-constraints-uw-ios-hw3
+//
+//  Created by Parker, Carl (HBO) on 4/25/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResponsiveViewController : UIViewController
+
+@end
---
+++ ResponsiveViewController.m
@@ -0,0 +1,37 @@
+//
+//  ResponsiveViewController.m
+//  layout-constraints-uw-ios-hw3
+//
+//  Created by Parker, Carl (HBO) on 4/25/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import "ResponsiveViewController.h"
+
+@interface ResponsiveViewController ()
+
+@end
+
+@implementation ResponsiveViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
